d4d8b1aee1c3edf2fcd55a5587c2a1da4a15a0d0
oningangela-commits <oning.angela@dnsc.edu.ph>
2026-07-27 11:47:36 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Scroll Animation */
+
+section{
+    opacity:0;
+    transform:translateY(50px);
+    transition:all .8s ease;
+}
+
+section.show{
+    opacity:1;
+    transform:translateY(0);
+}
